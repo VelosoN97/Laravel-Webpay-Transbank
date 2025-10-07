9f461e9452c9852797e05f79f53b6f045f3686d4
Nicolas Veloso <VelosoN97@users.noreply.github.com>
2025-10-07 18:38:03 -0300

    Commit 2 - Creación del archivo transbank.php para la vinculación con ENV

---
+++ config/transbank.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'environment' => env('TRANSBANK_ENV', 'integration'),
+    'webpay' => [
+        'commerce_code' => env('WEBPAY_COMMERCE_CODE', '597055555532'),
+        'api_key' => env('WEBPAY_API_KEY', '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'),
+    ],
+];
