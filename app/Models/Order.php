81c337724d25c872cd9c24842454beab14a05090
Nicolas Veloso <VelosoN97@users.noreply.github.com>
2025-10-08 12:21:32 -0300

    Commit 4 - Edición del modelo Order y creación del controlador TransbankController

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'buy_order',
+        'session_id',
+        'amount',
+        'status',
+        'authorization_code',
+        'payment_type',
+        'transaction_date',
+        'raw_response',
+    ];
+
+    protected $casts = [
+        'raw_response' => 'array',
+        'transaction_date' => 'datetime',
+    ];
+    
 }
