c2ff3fe6ccbdf38758f6682ecd9c8234312a45c4
Nicolas Veloso <VelosoN97@users.noreply.github.com>
2025-10-09 01:04:46 -0300

    Commit 12 - Modificación en el archivo de rutas dejando la vista buy como vista principal del proyecto

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\TransbankController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/buy', [TransbankController::class, 'buy'])->name('buy');
+Route::get('/', [TransbankController::class, 'buy'])->name('buy');
 Route::post('/pay', [TransbankController::class, 'pay'])->name('pay');
 Route::post('/confirm', [TransbankController::class, 'confirm'])->name('confirm');
 Route::get('/success', [TransbankController::class, 'success'])->name('success');
